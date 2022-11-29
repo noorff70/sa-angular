d5f6ea3038ce711fb04fb63879c57846f5d45094
Faizul Noor <faizul.noor@hotmail.com>
2022-11-29 17:49:26 -0500

    Add new environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8081'
-  //apiUrl: 'http://172.31.19.160:8080'
+  //apiUrl: 'http://localhost:8081'
+  apiUrl: 'http://3.12.185.202:8081'
 };
