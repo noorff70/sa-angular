3406766989e26e03e3c48d54a639c689216983b0
Faizul Noor <faizul.noor@hotmail.com>
2023-06-22 21:44:02 -0400

    Update something

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://api.studyaid.ca' // rest endpoint in aws
-  apiUrl: 'http://localhost:8081'
-  // apiUrl: 'https://3.12.185.202:8081' //elastic ip
+  apiUrl: 'https://api.studyaid.ca' ,// rest endpoint in aws
+  //apiUrl: 'http://localhost:8081',
+  // apiUrl: 'https://3.12.185.202:8081', //elastic ip
   // apiUrl: 'http://10.0.2.198:8081' // internal ip
   //apiUrl: 'https://172.17.0.1:8081' // internal ip
+  stripePublicKey: 'pk_test_51NAjB1GxFE5twCSxwjjzVzEVKPPZCpSHKdJupaOmtIVLXxtmJh2VSyF6aTp48P10AogGCwL7zEM7YgAnn9aWMTzv00GmJRiugp'
 };
